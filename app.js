10a9fcb925432ae765a8aa6784c21cc91768349e
Muhammad Haris Rahat <altafharis87@gmail.com>
2023-09-01 03:59:16 +0500

    index.html

--- app.js
+++ app.js
@@ -27,3 +27,61 @@
 
 // alert("Hello....I can run JS through my web browser's console")
 
+
+
+// TODO Chapter # 2
+
+
+// Question # 1
+
+// var userName
+// alert(userName)
+
+
+// Question # 2
+
+// var myName = "Muhammad Haris Rahat"
+// alert(myName)
+
+// Question # 3
+
+// var message = "My World"
+// alert(message)
+
+
+// Question # 4
+
+
+// var studentName = "Jhone Doe"
+// var age = "15 years old"
+// var qualification = "Certified Mobile Application Development"
+// alert(studentName)
+// alert(age)
+// alert(qualification)
+
+
+// Question # 5
+
+// var a = "PIZZA \nPIZZ\nPIZ\nPI\nP"
+// alert(a)
+
+
+// Question # 6
+
+// var a = "My Email Address is "
+// var email = "altafharis87@gmail.com"
+// alert(a+email)
+
+
+// Question # 7
+// var a = "I am trying to learn from the book "
+// var book ="A Smarter Way To Learn JavaScript"
+// alert(a+book)
+
+
+// Question # 9
+
+// var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(design)
+
+
