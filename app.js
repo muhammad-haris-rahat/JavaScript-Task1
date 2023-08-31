76851b82c576a15f3a9a7264ddbd340820f1ae25
Muhammad Haris Rahat <altafharis87@gmail.com>
2023-09-01 02:47:52 +0500

    index.html

---
+++ app.js
@@ -0,0 +1,29 @@
+// TODO Chapter # 1
+
+// Question # 1
+
+
+// alert("Welcome To My Website")
+
+
+// Question # 2
+
+
+// alert("Error! Please enter a valid password")
+
+
+// Question # 3
+
+// alert("Welcome to JS Land....\nHappy Coding!")
+
+// Question # 4
+
+// alert("Welcome to JS land....")
+// alert("Happy Coding!")
+
+
+// Question # 5
+
+
+// alert("Hello....I can run JS through my web browser's console")
+
